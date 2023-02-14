b853136e5a1a75c59a7b024dc41c380c2bfd528f
GrigoriyBabikhin <Grigoriy.Babikhin@gmail.com>
2023-02-14 18:25:14 +0500

    Task 54-56

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/55-understanding-undefined-value-returned-from-a-function.js
@@ -0,0 +1,18 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+  console.log(sum);
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+  console.log(sum)
+}
+// Only change code above this line
+
+addThree();
+addFive();
